ce2ca3d409962ee37a74df626b298943640d03d6
samira <95604232+SN-nasiri@users.noreply.github.com>
2025-08-05 11:46:39 +0330

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,1 +1,26 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
+def build_lstm_model(input_shape, output_dim):
+    """
+    Build and compile an LSTM model.
+
+    Parameters:
+    - input_shape: tuple, shape of the input data (timesteps, features)
+    - output_dim: int, number of output classes
+
+    Returns:
+    - Compiled Keras model
+    """
+    model = Sequential()
+    model.add(LSTM(64, input_shape=input_shape, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(output_dim, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
 
